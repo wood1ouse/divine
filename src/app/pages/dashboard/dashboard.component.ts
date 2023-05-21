9f47b000af8f5da1bf6692cce1daa19bf364d024
wood1ouse <m.mamontov-2024@kpi.ua>
2023-05-22 00:50:10 +0300

    DIVINE-11 * load projects and check link with trello on dashboard and email confirm improved

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TrelloFacade } from '@facades/trello.facade';
+import { ProjectsFacade } from '@facades/projects.facade';
 
 @Component({
   selector: 'divine-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  constructor(
+    private trelloFacade: TrelloFacade,
+    private projectsFacade: ProjectsFacade
+  ) {}
+
+  async ngOnInit() {
+    this.trelloFacade.dispatchCheckLinkWithTrello();
+
+    this.projectsFacade.dispatchLoadProjects();
+  }
+}
